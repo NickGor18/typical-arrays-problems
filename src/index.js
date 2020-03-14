41fef5730ff8d4997f8b319c60169169c048c4b4
NickGor18 <44360550+NickGor18@users.noreply.github.com>
2020-03-14 18:39:14 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
+  if(array==undefined)
   return 0;
+  var min = Math.min.apply(null, array);
+  if(min == Infinity)
+  return 0;
+  return min;
 }
 
 exports.max = function max (array) {
+  if(array==undefined)
   return 0;
+  var max = Math.max.apply(null, array);
+  if(max == -Infinity)
+  return 0;
+  return max;
 }
 
 exports.avg = function avg (array) {
+  if(array==undefined)
   return 0;
+  let k = 0;
+  for(let i=0; i<array.length; i++){
+    if(array[i]==undefined){
+      return 0;
+    }
+    k+=array[i];
+  }
+  if(Number.isNaN(k/array.length))
+  return 0;
+  return k/array.length;
 }
